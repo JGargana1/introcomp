d8b4839c37c0c5c2f315ddd1d56503e7469dab08
“Jeremy <jeremy.gargana1@marist.edu>
2019-09-12 15:14:44 -0400

    Solution to Curiosity rover exercise.

---
+++ lab02/rover.py
@@ -0,0 +1,9 @@
+def main():
+    print("The Mars Curiosity Rover takes photos of the Mars surface and then transmits them to NASA at the speed of light. Light travels roughly 186,000 miles per second.")
+    print("If a picture was taken 34,000,000 miles away from the earth, it would take:")
+    speed = 186,000
+    distance = 34,000,000
+    time = distance/speed 
+    print(time)
+    
+main()
