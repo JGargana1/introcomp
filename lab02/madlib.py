aad57980ba756590f2cf5a737ab16159b4ad722d
“Jeremy <jeremy.gargana1@marist.edu>
2019-09-18 23:41:30 -0400

    Solution to Mad Libs exercise.

---
+++ lab02/madlib.py
@@ -0,0 +1,9 @@
+def main():
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    place = input("Enter a place: ")
+    adjective = input("Enter an adjective: ")
+
+    print("There was a" + noun + "who started to" + verb + "at" + place + "with a" + adjective + "sock")
+
+main()    
