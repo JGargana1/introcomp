b6fe966b102841000e21dd2b72057d14e4df29a4
“Jeremy <jeremy.gargana1@marist.edu>
2019-09-18 23:51:00 -0400

    Solution to Mad Libs exercise.

--- lab02/madlib.py
+++ lab02/madlib.py
@@ -1,9 +1,10 @@
+#This program allows the user to create a Madlib.
 def main():
     noun = input("Enter a noun: ")
     verb = input("Enter a verb: ")
     place = input("Enter a place: ")
     adjective = input("Enter an adjective: ")
 
-    print("There was a" + noun + "who started to" + verb + "at" + place + "with a" + adjective + "sock")
+    print("There was a " + noun + " who started to " + verb + " at " + place + " with a " + adjective + " sock. ")
 
 main()    
